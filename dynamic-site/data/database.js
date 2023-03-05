3623cafff4ef00643977c4bb72d7a8d09e7d879d
jeongsangtae <wjdtkdxo55@naver.com>
2023-03-06 04:29:26 +0900

    NodeJS & MongoDB - Test & code prep (including environment variables)

--- dynamic-site/data/database.js
+++ dynamic-site/data/database.js
@@ -1,17 +1,23 @@
-const mongodb = require('mongodb');
+const mongodb = require("mongodb");
 
 const MongoClient = mongodb.MongoClient;
 
+let mongodbUrl = "mongodb://127.0.0.1:27017";
+
 let database;
 
+if (process.env.MONGODB_URL) {
+  mongodbUrl = process.env.MONGODB_URL;
+}
+
 async function initDatabase() {
-  const client = await MongoClient.connect('mongodb://localhost:27017');
-  database = client.db('deployment');
+  const client = await MongoClient.connect(mongodbUrl);
+  database = client.db("deployment");
 }
 
 function getDb() {
   if (!database) {
-    throw new Error('No database connected!');
+    throw new Error("No database connected!");
   }
 
   return database;
